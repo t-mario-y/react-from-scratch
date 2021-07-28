69e8f443cb97b65fee1c626691408d2c535dd00e
t-mario-y <t.mario.yoshi@gmail.com>
2021-07-29 03:46:40 +0900

    build: add minimal webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [{ loader: "ts-loader" }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".css"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "/src/index.html",
+    }),
+  ],
+};
