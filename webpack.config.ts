e34bfcbeef09d74c0998742dff0d32c07788fd30
t-mario-y <t.mario.yoshi@gmail.com>
2021-07-30 08:55:29 +0900

    feat: use webpack.config.ts

---
+++ webpack.config.ts
@@ -0,0 +1,60 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { WebpackOptionsNormalized } from "webpack";
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config: WebpackOptionsNormalized = {
+  entry: {
+    main: {
+      import: ["./src/index.tsx"],
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    defaultRules: ["..."],
+    generator: {},
+    parser: {},
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".css"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  cache: {
+    type: "filesystem",
+  },
+  devServer: {
+    contentBase: "dist",
+    open: true,
+  },
+  node: {},
+  infrastructureLogging: {},
+  optimization: {},
+  resolveLoader: {},
+  snapshot: {},
+  stats: {},
+  watchOptions: {},
+  experiments: {},
+  externals: {},
+  externalsPresets: {},
+};
+
+if (isProduction) {
+  config.mode = "production";
+} else {
+  config.mode = "development";
+}
+
+export default config;
